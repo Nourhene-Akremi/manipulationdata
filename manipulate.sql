ab19743cda4cd20d9393fa0879add63a459e3bdf
unknown <akreminourhene@gmail.com>
2022-02-28 15:04:43 +0100

    manipulation data

---
+++ manipulate.sql
@@ -0,0 +1,14 @@
+INSERT INTO Products(ProductId, ProductName,Category,Price) VALUES (P01,Samsung Galaxy S20,Smartphone,3299)
+
+INSERT INTO Products VALUES (P02,ASUS Notebook,PC,4599)
+
+INSERT INTO Products VALUES (C01,Ali,71321009)
+
+INSERT INTO Products VALUES (C02,Asma,773245823)
+
+INSERT INTO Orders (OderDate,Quantity,Total_amount)VALUES (C02,Asma,Null,2,9198)
+
+INSERT INTO Orders (OderDate,Quantity,Total_amount)VALUES (28/05/2020,1,3299)
+
+
+
